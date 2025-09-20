914bc71863a83c36441f928cbee87e2acad73066
Spokay <hugobressange@gmail.com>
2025-09-20 16:18:38 +0200

    fix: fixing the env variables load for vite

--- client/src/configuration/config.ts
+++ client/src/configuration/config.ts
@@ -1,4 +1,4 @@
-const BACKEND_BASE_URL = process.env.BACKEND_URL || 'http://localhost:3000';
+const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
 export {
     BACKEND_BASE_URL
